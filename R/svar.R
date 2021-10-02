b260c42afdadc5c5d7158faba78be9b4a03a0990
Chris Sims <sims@princeton.edu>
2021-10-02 14:47:22 -0400

    Cosmetic, except for removing tacking of T onto breaks, to match other programs.

--- R/svar.R
+++ R/svar.R
@@ -1,26 +1,26 @@
+#' SVAR estimation
+#' 
 #' Structural VAR estimation with time-varying shock variances
 #'
-#' @details Works from a data matrix that includes any dummy observations implementing
+#' Works from a data matrix that includes any dummy observations implementing
 #' a conjugate prior. Usually called from `svmdd()` rather than directly.
 #' This program does not set a prior distribution:  It assumes the prior has
 #' been implemented with dummy observations that are included in `ydata`.
-#' \subsection{breaks}{The first \code{lags} rows of `ydata` after an
+#' \subsection{`breaks`}{The first \code{lags} rows of `ydata` after an
 #'           element of `breaks` are used as new initial conditions, not
 #'           left-hand-side predicted values. This allows for gaps in the time
 #'           series and for the possibility of adding dummy observations to
 #'           implement a prior.} 
-#' \subsection{\code{A0}{constant matrix such that \code{A0 \% * \% u},
+#' \subsection{`A0`}{constant matrix such that \code{A0 \% * \% u},
 #'     where \code{u} is the reduced form VAR residual, has a diagonal covariance
-#'     matrix.
-#'     \code{lmd}: nvar by m matrix of relative variances of structural shock.
-#' \subsection{\code{Tsigbrk}}{vector of observation numbers after which the model
-#'          switches to the next column of \code{lmd}.  The rows of `lmd` are
-#'          constrained to add to one, and rows after the last element of
-#'          `Tsigbrk` all are given a vector of ones as `lmd` entries.}
-#'}
+#'     matrix.}
+#' \subsection{Varying variances`}{ The rows of `lmd` are constrained to average
+#'     to 1, and rows after the last element of `Tsigbrk` all are given a vector
+#'     of ones as `lmd` entries.}
+#'
 #'@param ydata Endogenous variable data matrix, including initial condition
 #'              dates.  Usually just an mts object.  More generally, it may
-#'              be a list of mts objects that will be stacked up for estimation.'
+#'              be a list of mts objects that will be stacked up for estimation.
 #' @param lags number of lags in the model
 #' @param xdata T x nx exogenous variable data matrix. 
 #' @param breaks Column vector of row numbers in ydata and xdata after which
@@ -28,12 +28,13 @@
 #' @param A0 nvar x nvar matrix of coefficients on contemporary variables.
 #' @param Tsigbrk Each entry of this vector indexes the last row of `ydata` in
 #'                the corresponding variance regime.
-#' @param lmd `nvar x length(Tsigbrk)` matrix of relative variances in regimes.
+#' @param lmd nvar x length(Tsigbrk) matrix of relative variances in regimes.
 #'            Each row normalized to average to one across the real data.
-#' @param nonorm When TRUE, marginal data density is not calculated. 
+#' @param nonorm When `TRUE`, marginal data density is not calculated. 
 #'               Can save a little time if mdd not needed, and is necessary if
 #'               dummy observations do not determine a proper prior.
-#'  @return  
+#' @return
+#' \describe{
 #'     \item{By}{nvar x nvar x lags matrix of coefficients on lagged `y`'s.
 #'     1st dimension is equation number}
 #'    \item{Bx}{nvar x nx matrix of coefficients on x}
@@ -42,26 +43,30 @@
 #'    \item{uraw}{Unweighted reduced-form residuals --- one-step-ahead
 #'       prediction errors}
 #'    \item{xxi}{Cross-product of weighted `X` matrix.  One for each equation.
-#'        Equation `i`'s right-hand-side coefficient covariance matrix. 
+#'        Equation `i`'s right-hand-side coefficient covariance matrix.}
 #'    \item{snglty}{ Usually 0.  If the rhs variable matrix is not full column
 #'       rank, this is the gap between the number of columns and the
-#'       number of non-zero singular values.
+#'       number of non-zero singular values.}
 #'}
+#' @seealso [svmdd()]
 #'@export
 #'@md
 #' 
 svar <- function(ydata=NA,
-             lags=6,
-             xdata=NULL,
-             const=TRUE,
-             breaks=NULL,
-             A0=diag(NCOL(ydata)),
-             Tsigbrk=NROW(ydata),
-             lmd=matrix(1, NCOL(ydata), length(Tsigbrk)),
-             nonorm=FALSE) {
+                 lags=6,
+                 xdata=NULL,
+                 const=TRUE,
+                 breaks=NULL,
+                 A0=diag(NCOL(ydata)),
+                 Tsigbrk=NROW(ydata),
+                 lmd=matrix(1, NCOL(ydata), length(Tsigbrk)),
+                 nonorm=FALSE) {
+    ## Tsigbrk here indexes the ends of all the regimes in real data
+    ## Its length thus does not account for the "regime"
+    ## in the dummy observations.  
     if (is.null(dim(ydata))) {
         dim(ydata) <- c(length(ydata),1)
-        }
+    }
     T <- dim(ydata)[1]
     ## Note that if svar() has been called with dummy obs's already in place, this T
     ## includes the dummies.
@@ -87,11 +92,11 @@ svar <- function(ydata=NA,
     else {
         nbreaks <- length(breaks)
     }
-    breaks <- c(0,breaks,T)
+    breaks <- c(0,breaks)               #breaks includes end of data
     if(any(breaks[2:length(breaks)] < breaks[1:(length(breaks)-1)]))
         stop("list of breaks must be in increasing order\n")
     smpl <- NULL
-    for (nb in 2:(nbreaks + 2)) {
+    for (nb in 2:(nbreaks + 1)) {
         ## if ( breaks[nb] > breaks[nb-1] + lags )
         ## Because data is already stacked up, every batch of data after
         ## a break has at least lags +1 rows.
@@ -106,12 +111,17 @@ svar <- function(ydata=NA,
     y <- ydata[smpl,,drop=FALSE]
     ## Everything now set up with input data for y=Xb+e 
     if (!is.null(Tsigbrk)) {
-        ## Tsigbrk <- invtime(Tsigbrk, ydata) #so Tsigbrk given as dates
         nsig <- length(Tsigbrk)
     } else {
         nsig <- 1
     }
-    Tsigbrk <- c(Tsigbrk, T)
+    if (T > Tsigbrk[nsig] ) {            #so there are some dummy obs
+        lmd <- cbind(lmd, rep(1, nvar))
+        Tsigbrk <- c(0, Tsigbrk, T)
+        nsig <- nsig + 1
+    } else {
+        Tsigbrk <- c(0,Tsigbrk)
+    }
     lmdndx <- rep(1:nsig, times=diff(Tsigbrk))
     lmdseries <- lmd[ , lmdndx]
     if ( Tsmpl < dim(y)[1] ) {      #dummy obs formed in rfvar3
@@ -159,9 +169,9 @@ svar <- function(ydata=NA,
     if (nox) {
         Bx <-  NULL
     } else {
-            Bx <- matrix(B[nvar * lags + (1:nx), ], nx, nvar)
-            Bx <- t(Bx)
-      }
+        Bx <- matrix(B[nvar * lags + (1:nx), ], nx, nvar)
+        Bx <- t(Bx)
+    }
     return(list(By=By, Bx=Bx, u=u, uraw=uraw, xxi= xxi, snglty=snglty, logdetxxi=logdetxxi,
-                    lmdseries=lmdseries, call=match.call()))
+                lmdseries=lmdseries, call=match.call()))
 }
