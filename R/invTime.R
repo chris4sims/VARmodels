f6685a4a92479b2130aad324a2caf9baedbe0ace
Chris Sims <sims@princeton.edu>
2021-10-02 14:43:20 -0400

    No longer make tricky attempt to distinguish integer-pair dates from real numbers.  Integer-pairs must now be in a 2-column matrix.

--- R/invTime.R
+++ R/invTime.R
@@ -10,35 +10,22 @@
 #'           dates in year-month pairs `c(1973,2), c(1990,9)` (for monthly data, e.g.).
 #'           If the date is outside the start, end range, the returned observation
 #'           number will be zero or N+1 where N is the number of rows in tsobj.
+#'           To avoid ambiguity, A single date of the form `c(1990,2)` must be
+#'           dimensioned as a 1 by 2 matrix.
 #' @export
 #' @md
 #' 
 invTime <- function(tsdate, tsobj) {
-  start <- tsp(tsobj)[1]
-  end <- tsp(tsobj)[2]
-  freq <- tsp(tsobj)[3]
-  tsobjInt <- time(tsobj)        
-  tsobjInt <- c(tsobjInt[1] - .5/freq, tsobjInt + .5 / freq)
-  if (is.null(dim(tsdate)) && length(tsdate)==2) { # ambiguity
-                                        # c(1900. 1900.25)
-                                        # or c(1900,1) ?
-    d2 <- tsdate[1] + (tsdate[2] - 1) / freq
-    d2OK <- d2 >= start   && d2 <= end 
-    d1OK <- all((tsdate >= start ) & (tsdate <= end) )
-    if (d2OK && d1OK) {
-      warning("ambiguous date")         #Should be rare!
-      obsno <- findInterval(d2, tsobjInt)
-    } else {                            # A single tsdate
-        obsno <- ifelse( d2OK, findInterval(d2, tsobjInt),
-                      findInterval(tsdate,tsobjInt))
-    }
-  } else {                              
+    start <- tsp(tsobj)[1]
+    end <- tsp(tsobj)[2]
+    freq <- tsp(tsobj)[3]
+    tsobjInt <- time(tsobj)        
+    tsobjInt <- c(tsobjInt[1] - .5/freq, tsobjInt + .5 / freq)
     if (is.null(dim(tsdate))) {         # dates as single real numbers
-      obsno <- findInterval(tsdate, tsobjInt)
+        obsno <- findInterval(tsdate, tsobjInt)
     } else {                            # dates as a 2-column array
-      tsd <- tsdate[ ,1] + (tsdate[ ,2] -1)/freq
-      obsno <- findInterval(tsd,  tsobjInt)
+        tsd <- tsdate[ ,1] + (tsdate[ ,2] -1)/freq
+        obsno <- findInterval(tsd,  tsobjInt)
     }
-  }
-  return(obsno)
+    return(obsno)
 }
