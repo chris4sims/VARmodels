f4a78436dc3a1b2f88b5d178b7f8de3d9051e056
Chris Sims <sims@princeton.edu>
2021-10-05 22:10:45 -0400

    vec2alm returned array with first index draws, which prints out badly.  Now draws is last index.

--- R/vec2alm.R
+++ R/vec2alm.R
@@ -6,5 +6,8 @@ vec2alm <- function(x, nv) {
     nSig <- (nx - nv^2) / nv + 1
     lmd <- array(x[ , -(1:nv^2)], c(ndraw, nv, nSig -1))
     lmd <- abind(lmd, nSig - apply(lmd, 1:2, sum), along=3)
+    A <- aperm(A, c(2, 3, 1))
+    lmd <- aperm(lmd, c(2, 3, 1))
+    ## permutation makes printout look better for small ndraw
     return(list(A=A, lmd=lmd))
 }
