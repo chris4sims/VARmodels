58ad3296299d7ff7e6b5c537f6e3021bd70d263c
Chris Sims <sims@princeton.edu>
2021-10-14 21:42:54 -0400

    roxygen documentation comments added to vec2alm

--- R/vec2alm.R
+++ R/vec2alm.R
@@ -1,3 +1,18 @@
+#'Retrieve `A0` and lmd matrices from `x`
+#'
+#' [svarwrap()] takes `A0` and `lmd` collapsed into an `x` vector as argument.
+#' This function reconstructs `A0` and `lmd` from `x`.
+#'
+#' @details The function  handles both the case of a single `x` vector and
+#' that of a matrix `x`, each row of which is expanded. The matrix case
+#' applies to the output from [almdDraw()]
+#'
+#' @return \describe{
+#'                   \item{A}{`nv` by `nv` by `ndraw `A0` array}
+#'                   \item{lmd}{`nv` by `nSig` by `ndraw` `lmd` array
+#' @export
+#' @md
+#' 
 vec2alm <- function(x, nv) {
     if (!is.matrix(x) || NCOL(x) == 1)  x <- matrix(x, nrow=1)
     ndraw <- dim(x)[1]
