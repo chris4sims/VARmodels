4916d999c4edb467e62077dedf1febe8c899b08a
Chris Sims <sims@princeton.edu>
2021-10-30 21:30:21 -0400

    fcastBand now uses the last element of MCMC draw arrays as the draw index, as do other programs in the package.  Documentation of smat and A0 use is clarified.  The whichs parameter was not used, and now is.

--- R/fcastBand.R
+++ R/fcastBand.R
@@ -6,9 +6,11 @@
 #' @param pdout  Output from `postdraw` or `SVARpostdraw()`.
 #' @param regime An integer, indicating column of lmd to use for relative shock
 #'               variances. 0 means use 1's (the "average"). Leave at 0 for rf case.
-#' @param smat If NULL, the transpose of \code{pdout$smat[ , , draw]} is used as
-#'              initial shocks.  Otherwise, this array is used, not transposed. Can be
-#'              a single array, which is used repeatedly.
+#' @param smat If rf case, if `smat` is non-NULL, it is used, either as a single
+#'             matrix or as an array varying with the draw, to determine the
+#'             shock distribution. If `smat` NULL (the default), the transpose of
+#'             `pdout$smat[ , , draw]` is used as initial shocks.   In the svar case,
+#'             the inverse of `pdout$A` is used and `smat` is ignored.  
 #' @param y0 Initial conditions for forecast. Its tsp() is used, if present.
 #' @param nstep Number of steps ahead to calculate forecasts.
 #' @param pctiles At what percentiles to draw bands. (vector of odd length)
@@ -39,11 +41,11 @@ fcastBand <- function(pdout, regime=0, y0, horiz=40, pctiles=c(5, 16, 50, 84, 95
     if (!is.null(pdout$A) ) {           #SVAR case
         smat <- array(0, c(nv, nv, ndraw))
         for (id in 1:ndraw) {
-            smat[ , , id] <- solve(pdout$A[id, , ])
-            if(regime > 0) smat[ , , id] <- smat[ , , id] %*% diag(pdout$lmd[id, , regime])
+            smat[ , , id] <- solve(pdout$A[ , , id])
+            if(regime > 0) smat[ , , id] <- smat[ , , id] %*% diag(pdout$lmd[ , regime, id])
         }
-        dimnames(smat) <- list(var=dimnames(pdout$A)[[3]],
-                               shock=dimnames(pdout$A)[[2]],
+        dimnames(smat) <- list(var=dimnames(pdout$A)[[2]],
+                               shock=dimnames(pdout$A)[[1]],
                                NULL)
     } else {                             #rf case
         if (!is.null(smat)) {               #using explicit smat argument
@@ -71,11 +73,12 @@ fcastBand <- function(pdout, regime=0, y0, horiz=40, pctiles=c(5, 16, 50, 84, 95
         dim(By) <- dim(By)[1:3]         #in 1x1 case, need to keep initial 3d, not 4th
         if(!identical(whichs,0)) {
             shocks <- matrix(rnorm(nv * horiz), horiz, nv)
-            shocks <- shocks %*% smat[ , , id]
+            shocks <- (shocks %*% smat[ , , id])[ , whichs]
         } else {
             shocks <- NULL
         }
-        fc[ , , id] <- fcast(y0, By, pdout$Bx[ , , id], xdata=xdata, const=const, horiz=horiz, shocks=shocks)
+        fc[ , , id] <- fcast(y0, By, pdout$Bx[ , , id],
+                             xdata=xdata, const=const, horiz=horiz, shocks=shocks)
     }
     fcSorted <- apply(fc[ , whichv, , drop=FALSE], 1:2, sort)
     fcSorted <- aperm(fcSorted, c(2,3,1))
