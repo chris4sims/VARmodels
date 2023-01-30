314c3c667394058c3c6a5c35f751fe865eaeeb9c
Chris Sims <sims@princeton.edu>
2023-01-29 21:48:31 -0500

    vec2alm now returns variable labels on A and lmd

--- R/vec2alm.R
+++ R/vec2alm.R
@@ -13,19 +13,26 @@
 #' @export
 #' @md
 #' 
-vec2alm <- function(x, nv) {
+vec2alm <- function(x, nv, vnames=NULL) {
     if (!is.matrix(x) || NCOL(x) == 1)  x <- matrix(x, nrow=1)
     ndraw <- dim(x)[1]
     nx <- dim(x)[2]
     A <- array(x[ , 1:nv^2], c( ndraw, nv, nv))
     nSig <- (nx - nv^2) / nv + 1
     lmd <- array(x[ , -(1:nv^2)], c(ndraw, nv, nSig -1))
-    lmd <- abind(lmd, nSig - apply(lmd, 1:2, sum), along=3)
+    ## ----------for Dirichlet prior on lmd where rows are mean 1
+    ## lmd <- abind(lmd, nSig - apply(lmd, 1:2, sum), along=3)
+    ##----------------------------------------------------
+    lmd <- abind(lmd, exp(-apply(log(lmd), 1:2, sum)), along=3)
     A <- aperm(A, c(2, 3, 1))
     lmd <- aperm(lmd, c(2, 3, 1))
     if (ndraw == 1) {
       A <- A[ , , 1]
       lmd <- lmd[ , , 1]
+    }
+    if(!is.null(vnames)) {
+      dimnames(A)[[2]] <- vnamesDec
+      dimnames(lmd)[[1]] <- vnames
     }
     ## permutation makes printout look better for small ndraw
     return(list(A=A, lmd=lmd))
